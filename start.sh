064828ed80c66848d0f651cc15dfb88acb599dbe
Simon Stockhause <simon.stockhause@lhsystems.com>
2023-12-14 21:54:44 +0100

    fix: add debug info

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Checking ${TARGET_DATA_DIR}"
+echo "Crontabs"
+echo "$(crontab -l)"
 service cron start
 
 exec node_exporter --web.listen-address=:9200 --collector.disable-defaults --collector.textfile.directory=/var/lib/node_exporter --collector.textfile
