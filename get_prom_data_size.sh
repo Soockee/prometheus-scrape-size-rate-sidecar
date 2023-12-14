f38401053223a6da481349a08c657f35f187c6d5
Simon Stockhause <simon.stockhause@lhsystems.com>
2023-12-14 22:50:40 +0100

    fix: change file location

--- get_prom_data_size.sh
+++ get_prom_data_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "# HELP node_prometheus_data_size_kilobytes Size of specified folder in kilobytes
 # TYPE node_prometheus_data_size_kilobytes gauge
-node_prometheus_data_size_kilobytes $(du -s ${TARGET_DATA_DIR} 2>/dev/null | cut -f1)" >/var/lib/node_exporter/custom_metrics.prom
+node_prometheus_data_size_kilobytes $(du -s ${TARGET_DATA_DIR} 2>/dev/null | cut -f1)" >/custom_metrics.prom
